09c8ff600f6678782e895bf4be8a378d2530e357
unregistered_name <makcim1806@gmail.com>
2022-06-13 11:48:42 +0300

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,43 @@
+"""The game guess the number"""
+import numpy as np
+def random_predict(number:int=1) -> int:
+    """Random;y guessing the number
+
+    Args:
+        number (int, optional): hidden number. Defaults to 1.
+
+    Returns:
+        int: number of attempts
+    """
+    
+    count = 0
+    
+    while True:
+        count+=1
+        predict_number = np.random.randint(1,101) # Estimated number
+        if number == predict_number:
+            break # Exit the loop
+    return count
+
+#print(f'Number of attempts: {random_predict()}')
+def score_game(random_predict) -> int:
+    """The average number out of 1000 approaches
+
+    Args:
+        random_predict (_type_): guessing functuin
+
+    Returns:
+        int: average number of attempts
+    """
+    count_ls = [] # list for save numer of attempts
+    np.random.seed(1) # Fixing seed for reproducibility
+    random_array = np.random.randint(1,101,size=(1000)) # list of numbers
+    for number in random_array:
+        count_ls.append(random_predict(number))
+    score = int(np.mean(count_ls)) # find average number of attempts
+    print(f' Your algorithm guesse number in average of {score} appemtps')
+    return score
+
+if __name__ == '__main__':
+    #RUN
+    score_game(random_predict)
